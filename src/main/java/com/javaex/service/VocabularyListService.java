f9ade374336e764b9683061f939f18391f8d4c80
seokh1008 <seokh1008@naver.com>
2019-02-01 19:29:47 +0900

    se
    Signed-off-by: seokh1008 <seokh1008@naver.com>

--- src/main/java/com/javaex/service/VocabularyListService.java
+++ src/main/java/com/javaex/service/VocabularyListService.java
@@ -1,14 +1,23 @@
 package com.javaex.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.javaex.repository.VocabularyListDao;
+import com.javaex.vo.URLPathVo;
+import com.javaex.vo.WordbookVo;
 
 @Service
 public class VocabularyListService {
 
 	@Autowired
 	VocabularyListDao vocabularyListDao;
+	
+	//디렉토리 목록 보여주는 리스트
+//			public List<WordbookVo> getWordbookAlldirectoryList(URLPathVo urlPathVo){
+//				return addVocabularyDao.selectWordbookAlldirectoryList(urlPathVo);
+//			}
 	
 }
