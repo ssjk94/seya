d3e1c8f65e53e074bae56085275e659c5251b164
seokh1008 <seokh1008@naver.com>
2019-03-15 14:10:14 +0900

    랭킹
    Signed-off-by: seokh1008 <seokh1008@naver.com>

--- src/main/java/com/javaex/controller/RankingController.java
+++ src/main/java/com/javaex/controller/RankingController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.javaex.service.RankingService;
-import com.javaex.vo.PairSetGameVo;
 import com.javaex.vo.RankingVo;
 
 @Controller
@@ -22,12 +21,10 @@ public class RankingController {
 	@RequestMapping(value = "gamerankingpage", method = RequestMethod.POST)
 	public List<Object> getRankingMain(RankingVo rankingVo) {
 		System.out.println("랭킹 가져오기 ajax실행");
-		
-		
-		
-
-		
-		return null;//rankingSource;
+		//마이랭킹의 userScore
+		//랭킹 TOP5
+		List<Object> rankingSource = rankingService.getRanking(rankingVo);
+		return rankingSource;
 	}
 	
 }
